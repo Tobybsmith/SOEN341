bdf56e31e5fdea18df84d36a25f4e6fe4f2319d7
Tobybsmith <tobybsmith121@gmail.com>
2023-01-25 16:16:43 -0500

    Added SASS, started routing test

--- outlined/src/routes/+page.svelte
+++ outlined/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let temp = "page! :)"
+</script>
+
+<!--
+    SASS is being used to make CSS nicer.
+    Note how the text in the p is green,
+    but the nested a text is yellow!
+    Order does not matter, since cascading doesn't
+    happen with inheritance
+-->
+<style lang="scss">
+    h1
+    {
+        color: blueviolet;
+    }
+    p
+    {
+        a
+        {
+            color: goldenrod;
+        }
+        color: green;
+    }
+</style>
+
+<main>
+    <h1>
+        This is the landing {temp}
+    </h1>
+    <p>It is the first page someone will see when they navigate to our site!</p>
+    <p>Click <a href="main">ME</a> to go to the main page!</p>
+</main>
---
+++ outlined/src/routes/main/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    
+</script>
+
+<style lang="scss">
+
+</style>
+
+<main>
+    <h1>
+        This is the main page!
+    </h1>
+    <p>This is where all the career postings go!</p>
+</main>
