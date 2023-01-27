28e5a57f461998db2a3cbda360e5b4ffc12f440b
Tobybsmith <tobybsmith121@gmail.com>
2023-01-27 12:19:15 -0500

    Updated project to include test components

---
+++ outlined/src/components/test/Green.svelte
@@ -0,0 +1,13 @@
+<strong>This is some text!</strong>
+<p>Some more text hehe!</p>
+
+<style lang="scss">
+    strong
+    {
+        color: olivedrab;
+    }
+    p
+    {
+        color: orangered;
+    }
+</style>
--- outlined/src/routes/main/+page.svelte
+++ outlined/src/routes/main/+page.svelte
@@ -1,14 +1,42 @@
 <script>
-    
+    import Red from '../../components/test/Red.svelte'
+    import Green from '../../components/test/Green.svelte'
+
+    const options = 
+    [
+        {color: 'red', component: Red},
+        {color: 'green', component: Green},
+    ];
+
+    //Default
+    let selected = options[1];
 </script>
 
 <style lang="scss">
-
+    p
+    {
+        color: rgb(35, 68, 168);
+        a
+        {
+            color: orangered;
+        }
+    }
 </style>
 
 <main>
     <h1>
         This is the main page!
     </h1>
-    <p>This is where all the career postings go!</p>
+    <p>This is where all the postings go!</p>
+    <p>Click <a href="../">ME</a> to go back to the home page</p>
+
+
+    <select bind:value={selected}>
+        {#each options as option}
+            <option value={option}>{option.color}</option>
+        {/each}
+    </select>
+
+    <svelte:component this={selected.component}/>
+    
 </main>
