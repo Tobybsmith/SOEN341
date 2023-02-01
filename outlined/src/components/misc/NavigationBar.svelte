014680f673e2258bd849aafdb7e762c46ecf591f
Tobybsmith <tobybsmith121@gmail.com>
2023-02-01 16:34:54 -0500

    Created templates for Nav and Profile

---
+++ outlined/src/components/misc/NavigationBar.svelte
@@ -0,0 +1,13 @@
+<script>
+    //Nav bar will go here!
+    export let selected_page = "home";
+</script>
+
+<main>
+    <div class="bar">
+        <!--Conditional Logic in HTML? Wowee-->
+        <a href="." class="{selected_page === "home" ? "sel" : "no-sel"}">Home</a>
+        <a href="." class="{selected_page === "home" ? "sel" : "no-sel"}">Profile</a>
+        <a href="." class="{selected_page === "home" ? "sel" : "no-sel"}">Landing Page</a>
+    </div>
+</main>
