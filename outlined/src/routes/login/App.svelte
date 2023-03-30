93870a5f2b8dca482507e8c1d954af6c64e21cc4
Janice Jacqueline <Janicejacqueline@icloud.com>
2023-03-29 22:41:23 -0400

    login page again

--- outlined/src/routes/login/App.svelte
+++ outlined/src/routes/login/App.svelte
@@ -11,16 +11,34 @@
     };
 </script>
 
-<header class="p-3 bg-dark text-white">
+<header class="root">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
             </ul>
             <div class="text-end">
-                <a href="/login" class="btn btn-outline-light me-2">Login</a>
-                <a href="/register" class="btn btn-outline-light me-2">Register</a>
+                <a href="/login" class="main">Login</a>
+                <a href="/register" class="main">Register</a>
             </div>
         </div>
     </div>
 </header>
+
+<style>
+    :root
+{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+    --corner-curve: 25px;
+    --main-color: #eceff1;
+    --sec-color: #37474f;
+    --main-text: #212121;
+    --main-high: #f9a825;
+
+    background-color: var(--main-color);
+    color: var(--main-text);
+    margin: 0;
+    overflow-x: hidden;
+}
+</style>
