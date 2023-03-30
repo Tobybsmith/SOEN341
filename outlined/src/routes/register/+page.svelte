5a65c1683c8dac50a5850f2c9103174d90702365
Janice Jacqueline <Janicejacqueline@icloud.com>
2023-03-29 22:41:11 -0400

    update login page again

--- outlined/src/routes/register/+page.svelte
+++ outlined/src/routes/register/+page.svelte
@@ -1,39 +1,84 @@
 <script>
+    import { goto } from '$app/navigation';
     import axios from 'axios';
     import {push} from 'svelte-spa-router'
 
-    let name = ' ', email = ' ', password = ' '
+    let name = ' ', email = ' ', password = ' ', confirmpassword = ' '
 
     $:submit = async () => {
         await axios.post('register', {
             name,
             email,
-            password
+            password,
+            confirmpassword
         });
 
         await push ('/login');
     }
+
+    function GoToMain(){
+        goto("/main");
+    }
+
 </script>
 
-<main class="form-signin">
+<main class="root">
     <form on:submit|preventDefault={submit}>
-        <h1 class="h3 mb-3 fw-normal">Please register</h1>
-
+        <h1 class="main">You don't want to miss out! Register yourself here!</h1>
+        
+        <h2 class="main">Name</h2>
         <div class="form-floating">
-            <input bind:value={name} class="form-control" placeholder="Name">
-            <label><input type='text'>Name</label>
+            <input bind:value={name} class="main" placeholder="Full Name">
         </div>
 
+        <h2 class="main">Email Address</h2>
         <div class="form-floating">
-            <input bind:value={email} type="email" class="form-control" placeholder="name@example.com">
-            <label><input type='text'>Email address</label>
+            <input bind:value={email} type="email" class="main" placeholder="name@example.com">
         </div>
 
+        <h2 class="main">Password</h2>
         <div class="form-floating">
-            <input bind:value={password} type="password" class="form-control" placeholder="Password">
-            <label><input type='text'>Password</label>
+            <input bind:value={password} type="password" class="main" placeholder="Password">
         </div>
 
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+        <h2 class="main">Confirm Password</h2>
+        <div class="form-floating">
+            <input bind:value={confirmpassword} type="password" class="main" placeholder="Confirm Password">
+        </div>
+
+        <button class="button-container" type="submit" on:click|preventDefault={GoToMain}>Submit</button>
     </form>
 </main>
+
+<style>
+    :root
+    {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    
+        --corner-curve: 25px;
+        --main-color: #eceff1;
+        --sec-color: #37474f;
+        --main-text: #212121;
+        --main-high: #f9a825;
+    
+        background-color: var(--main-color);
+        color: var(--main-text);
+        margin: 0;
+        overflow-x: hidden;
+    }
+    .button-container
+    {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-end;
+    }
+    main
+    {
+        margin: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    </style>
